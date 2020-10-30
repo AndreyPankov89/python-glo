29bb91cfce815b09841b9d4d36c0f62075b2acd9
Andrey <apan89@mail.ru>
2020-10-30 20:13:27 +0500

    lesson 1

---
+++ less1.py
@@ -0,0 +1,48 @@
+# int and float
+a = 5
+b = 4.5
+c = a + b - 2 * 5 / 3 + 1 - 5 ** 2 + 4 % 25 // 3
+print(a, type(a))
+print(b, type(b))
+print(c, type(c))
+
+# string
+name = "Andrey"
+sername = "Pankov"
+sex = "Male"
+
+print(name, type(name))
+print(sername, type(sername))
+print(sex, type(sex))
+
+# set
+a = {1, 2, 3}
+b = {4,5,6}
+c={7,8,9}
+print(a, type(a))
+print(b, type(b))
+print(c, type(c))
+
+# list
+a = [1, 2, 3]
+b = [4,5,6]
+c=[7,8,9]
+print(a, type(a))
+print(b, type(b))
+print(c, type(c))
+
+# tuple
+a = (1, 2, 2, 3)
+b = (4,5,6)
+c=(7,8,9)
+print(a, type(a))
+print(b, type(b))
+print(c, type(c))
+
+# dict
+a = {1: 1, 2:1, 3:5}
+b = {4: '5',5:4,6:[1,5]}
+c={7:1,8:'8',9:'asdf'}
+print(a, type(a))
+print(b, type(b))
+print(c, type(c))
