ec4afb122cb864d627c538588db801ec07306b8f
Andrey <apan89@mail.ru>
2021-05-25 18:44:07 +0500

    lesson 10 fix

--- lesson10/task4.py
+++ lesson10/task4.py
@@ -1,10 +1,14 @@
 chr_a = ord('a')
 chr_z = ord('z')
+chr_A = ord('A')
+chr_Z = ord('Z')
 
 character = ord(input('Введите символ'))
 
 if (character >= chr_a and character <= chr_z):
     character = chr(character).upper()
+elif (character >= chr_A and character <= chr_Z):
+    character = chr(character).lower()
 else:
     character = chr(character)
 print(character)
